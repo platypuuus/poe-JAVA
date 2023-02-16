2ca1d30610e6614d05bd8cf8486c798c396e7671
Platypuus <clement8.baille@gmail.com>
2023-02-16 16:16:36 +0100

    initial commit

---
+++ src/main/java/fr/aelion/modeles/Student.java
@@ -0,0 +1,49 @@
+package fr.aelion.modeles;
+
+public class Student {
+
+    public String lastName;
+    public String firstName;
+
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String username;
+    private String password;
+    private Boolean isLoggedIn = false;
+
+    public Student(String lastName, String firstName, String email) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+    public Boolean login(String username, String password) {
+        if (this.username.equals(username) && this.password.equals(password)) {
+            isLoggedIn = true;
+            return true;
+        }
+        return false;
+
+    }
+
+
+    public void logout() {
+        isLoggedIn = false;
+    }
+
+    public Boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+}
